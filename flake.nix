655d452a20e4274d87d9b600f60ff3f1351f2e0a
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-04 16:17:35 -0600

    fix: set correct url for nix packages

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Mahyar's Nix configurations.";
 
   inputs = {
-    nixpkgs.url = "github:nixpkgs/nixos-24.05";
+    nixpkgs.url = "nixpkgs/nixos-24.05";
   };
 
   outputs = { nixpkgs, ... }:
