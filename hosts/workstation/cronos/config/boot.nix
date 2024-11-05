d3bf00dbd062ef9deb1858bdcf3f8c0a68de8704
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-04 23:38:17 -0600

    refactor(cronos): switch to grub

--- hosts/workstation/cronos/config/boot.nix
+++ hosts/workstation/cronos/config/boot.nix
@@ -1,6 +1,7 @@
 { ... }: {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "uhci_hcd" "ehci_pci" "ahci" "usbhid" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
