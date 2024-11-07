0b98fa5d80d2528fe47008ee14e4e5d5cbc42e5c
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-07 17:00:08 -0600

    refactor: use simpler flake entrypoint

--- flake.nix
+++ flake.nix
@@ -1,58 +1,29 @@
 {
   description = "Mahyar's Nix configurations.";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-24.05";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+  };
 
   outputs =
     { nixpkgs, ... }:
     let
-      lib = nixpkgs.lib;
-
-      filterForDirectories =
-        dirContents: builtins.attrNames (lib.filterAttrs (_n: v: v == "directory") dirContents);
-
-      # Define the top-level hosts folder path (where all host configurations live)
-      hostsRootDir = ./hosts;
-
-      # Retrieve the role directories (e.g., "./hosts/workstations", "./hosts/servers")
-      getRolesInHostDir =
-        hostsRootDir:
-        map (role: "${hostsRootDir}/${role}") (filterForDirectories (builtins.readDir hostsRootDir));
-
-      # Retrieve the host directories (e.g. "./hosts/workstations/cronos", "./hosts/servers/alpha")
-      getHostsInRoleDir =
-        roleDir: map (host: "${roleDir}/${host}") (filterForDirectories (builtins.readDir roleDir));
-
-      # Retrieve the system architecture from the host configuration.nix
-      getSystemType =
-        host:
-        if builtins.pathExists "${host}/configuration.nix" then
-          (import "${host}/configuration.nix").system
-        else
-          lib.asserts.fail "Error: ${host} is missing the configuration.nix file.";
-
-      # Compute all hosts
-      hosts = builtins.concatMap getHostsInRoleDir (getRolesInHostDir hostsRootDir);
-
-      nixosHosts = builtins.filter (host: builtins.pathExists "${host}/system.nix") hosts;
+      nixosHosts = [
+        {
+          path = ./hosts/workstations/cronos;
+          system = "x86_64-linux";
+        }
+      ];
     in
     {
       nixosConfigurations = builtins.listToAttrs (
-        map (
-          host:
-          let
-            systemType = getSystemType host;
-          in
-          {
-            name = builtins.baseNameOf host;
-            value = nixpkgs.lib.nixosSystem {
-              system = systemType;
-              modules = [
-                (import "${host}/system.nix")
-              ];
-            };
-          }
-        ) nixosHosts
+        map (host: {
+          name = builtins.baseNameOf host.path;
+          value = nixpkgs.lib.nixosSystem {
+            system = host.system;
+            modules = [ host.path ];
+          };
+        }) nixosHosts
       );
     };
 }
