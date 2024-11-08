9d31e4d6bd52fd6aec9298ecf965e7c431d07442
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-07 22:00:20 -0600

    all: refactor declaration of additional modules

--- flake.nix
+++ flake.nix
@@ -4,10 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
