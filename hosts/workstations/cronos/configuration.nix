77ab4267a0d02f02ab79021b08eed5937b7f46d5
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-07 18:11:57 -0600

    cronos: add basic configuration

---
+++ hosts/workstations/cronos/configuration.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ./disk-configuration.nix
+    ./hardware-configuration.nix
+
+    pkgs.nixosModules.disko
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "cronos";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/Winnipeg";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+  ];
+
+  system.systemVersion = "24.05";
+}
