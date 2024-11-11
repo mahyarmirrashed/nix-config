2a848cfb9d10e053051421be948d1ca822926c0c
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-10 21:51:06 -0600

    all: add greetd configuration

--- nixosModules/desktop/loginManager/greetd/default.nix
+++ nixosModules/desktop/loginManager/greetd/default.nix
@@ -1,6 +1,45 @@
-{ config, lib, ... }:
 {
-  config = lib.mkIf (config.nixosModules.desktop.loginManager.selected == "greetd") {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.nixosModules.desktop.loginManager;
+in
+{
+  options.nixosModules.desktop.loginManager.greetd.greeter = lib.mkOption {
+    type = lib.types.enum [
+      "agreety"
+      "tuigreet"
+    ];
+    description = "Select the greeter to use.";
+    default = "agreety";
+  };
+
+  config = lib.mkIf (cfg.selected == "greetd") {
     services.greetd.enable = true;
+    # Parsed as a TOML file
+    # See: https://man.archlinux.org/man/greetd.5.en
+    services.greetd.settings = {
+      terminal = {
+        vt = 1;
+      };
+      default_session =
+        {
+          command = ''
+            ${lib.meta.getExe pkgs.greetd.tuigreet}
+            --cmd ${lib.meta.getExe pkgs.bashInteractive}
+          '';
+        }
+        / {
+          user = "greeter";
+        };
+    };
+
+    users.users.greeter = {
+      isSystemUser = true;
+      description = "User for greetd.";
+    };
   };
 }
