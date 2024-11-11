9a69765e1f0311345b9cec0eeaa48411722062c9
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-11 05:30:17 -0600

    all: add more hyprland session variables

--- nixosModules/desktop/windowManager/hyprland/default.nix
+++ nixosModules/desktop/windowManager/hyprland/default.nix
@@ -16,6 +16,11 @@ in
     programs.hyprland.enable = true;
 
     environment.systemPackages = with pkgs; [ kitty ];
-    environment.sessionVariables.NIXOS_OZONE_WL = "1";
+    environment.sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+      XDG_CURRENT_DESKTOP = "Hyprland";
+      XDG_SESSION_DESKTOP = "Hyprland";
+      XDG_SESSION_TYPE = "wayland";
+    };
   };
 }
