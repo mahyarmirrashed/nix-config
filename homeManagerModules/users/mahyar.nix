68080fea5dbf53cbcba193daac439235d25467f8
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-11 06:19:12 -0600

    chore: add correct hashed password

--- homeManagerModules/users/mahyar.nix
+++ homeManagerModules/users/mahyar.nix
@@ -7,7 +7,7 @@
     isNormalUser = true;
     extraGroups = [ "wheel" ];
 
-    hashedPassword = "$6$9KIgQ6x2cyLgRhQu$6LUUra8BGvTxj74gLMDl6QkgwKCvB9IA3.NH3g81/no08Xp.a/oRP5KpAMqR1z1g96LNqrBbduEhVYrWUbkS30";
+    hashedPassword = "$y$j9T$o85wa68FyAb0aDM79aWb.1$0kCK6ZFd5BY5x/ZUZAc7Qi9z8bLRbfOFFoZWqkULu/A";
   };
 
   home-manager.users.mahyar = {
