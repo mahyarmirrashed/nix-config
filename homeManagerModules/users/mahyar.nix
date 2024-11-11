408409cf2b0632709d7b039e095c6a12bd2efeaa
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-11 06:11:19 -0600

    all: fully specify mahyar user

--- homeManagerModules/users/mahyar.nix
+++ homeManagerModules/users/mahyar.nix
@@ -1,8 +1,13 @@
 { pkgs, ... }:
 {
   users.users.mahyar = {
+    createHome = true;
+    description = "Mahyar Mirrashed";
+
     isNormalUser = true;
     extraGroups = [ "wheel" ];
+
+    hashedPassword = "$6$9KIgQ6x2cyLgRhQu$6LUUra8BGvTxj74gLMDl6QkgwKCvB9IA3.NH3g81/no08Xp.a/oRP5KpAMqR1z1g96LNqrBbduEhVYrWUbkS30";
   };
 
   home-manager.users.mahyar = {
