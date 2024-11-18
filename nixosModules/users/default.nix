5496d00103802b3dd617bd21f0ed34755e1a9108
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-18 01:33:29 -0600

    feat(users): add ability to configure users on the fly

--- nixosModules/users/default.nix
+++ nixosModules/users/default.nix
@@ -1,12 +1,30 @@
-{ ... }:
-{
-  users.users.mahyar = {
-    createHome = true;
+{ config, lib, ... }:
+let
+  defaultUserConfig = {
+    createHome = lib.mkDefault true;
+    isNormalUser = lib.mkDefault true;
+  };
+
+  defaultUserSet = [ "mahyar" ];
+  defaultUserSetConfig.mahyar = {
     description = "Mahyar Mirrashed";
-
-    isNormalUser = true;
     extraGroups = [ "wheel" ];
-
     hashedPassword = "$y$j9T$o85wa68FyAb0aDM79aWb.1$0kCK6ZFd5BY5x/ZUZAc7Qi9z8bLRbfOFFoZWqkULu/A";
   };
+
+  cfg = lib.unique (config.modules.users ++ defaultUserSet);
+in
+{
+  options.modules.users = lib.mkOption {
+    type = lib.types.listOf lib.types.str;
+    default = [ "mahyar" ];
+    description = "List of system users to configure. 'mahyar' will always to be included.";
+  };
+
+  config.users.users = lib.mkMerge [
+    # Dynamically generate default configurations for all users
+    (lib.genAttrs cfg (_: defaultUserConfig))
+    # Apply specific overrides for default user set
+    defaultUserSetConfig
+  ];
 }
