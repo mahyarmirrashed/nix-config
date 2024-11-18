7732e09274301c6a63f9ddd53f2ba3da7be416c0
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-18 01:46:14 -0600

    feat(hyperion): add wezterm

--- hosts/workstations/hyperion/home.nix
+++ hosts/workstations/hyperion/home.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   modules.users = [ "mahyar" ];
 
@@ -8,5 +8,11 @@
     extraGroups = [ "wheel" ];
 
     hashedPassword = "$y$j9T$o85wa68FyAb0aDM79aWb.1$0kCK6ZFd5BY5x/ZUZAc7Qi9z8bLRbfOFFoZWqkULu/A";
+  };
+
+  home-manager.users.mahyar = {
+    home.packages = with pkgs; [
+      wezterm
+    ];
   };
 }
