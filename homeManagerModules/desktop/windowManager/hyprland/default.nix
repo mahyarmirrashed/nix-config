459ca9c125fa588f51c1b0144b4064fb6e459c38
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-17 20:11:38 -0600

    feat(hyprland): add bind for kitty

--- homeManagerModules/desktop/windowManager/hyprland/default.nix
+++ homeManagerModules/desktop/windowManager/hyprland/default.nix
@@ -1,4 +1,13 @@
-{ ... }:
+{ config, lib, ... }:
+let
+  cfg = config.modules.desktop.windowManager.hyprland;
+in
 {
-  imports = [ ];
+  wayland.windowManager.hyprland.settings = lib.mkIf cfg.enable {
+    "$mod" = "SUPER";
+
+    bindd = [
+      "$mod, K, Open terminal, exec, kitty"
+    ];
+  };
 }
