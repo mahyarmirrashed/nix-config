cbb03b02051f396bc148463249ecd26b118ee6dc
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-18 02:54:44 -0600

    feat(hyprpaper): add wallpaper

--- homeManagerModules/desktop/wallpaper/hyprpaper/default.nix
+++ homeManagerModules/desktop/wallpaper/hyprpaper/default.nix
@@ -10,6 +10,18 @@ in
   options.modules.desktop.wallpaper.hyprpaper.enable = lib.mkEnableOption "hyprpaper";
 
   config = lib.mkIf cfg.enable {
-    services.hyprpaper.enable = true;
+    services.hyprpaper = {
+      enable = true;
+
+      settings = {
+        preload = [
+          "${../wallpapers/gruvbox/eden.png}"
+          "${../wallpapers/gruvbox/rust.png}"
+          "${../wallpapers/gruvbox/space.png}"
+        ];
+
+        wallpaper = [ ",${../wallpapers/gruvbox/space.png}" ];
+      };
+    };
   };
 }
