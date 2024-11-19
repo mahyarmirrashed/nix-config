b3049b671b4d0515b682d45399d9b716c37a462a
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-18 20:23:18 -0600

    feat(terminals): add option for default terminal which must be set

--- homeManagerModules/applications/terminals/default.nix
+++ homeManagerModules/applications/terminals/default.nix
@@ -1,7 +1,13 @@
-{ ... }:
+{ lib, ... }:
 {
   imports = [
     ./alacritty
     ./wezterm
   ];
+
+  options.modules.applications.terminals.default = lib.mkOption {
+    type = lib.types.str;
+    default = null;
+    description = "The default terminal emulator for the system.";
+  };
 }
