b6dd6a411cc94cd4f04a7b7c3d11a4f62824f0fa
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-19 21:00:54 -0600

    feat(browsers): add option for default browser

--- homeManagerModules/applications/browsers/default.nix
+++ homeManagerModules/applications/browsers/default.nix
@@ -1,6 +1,32 @@
-{ ... }:
+{
+  config,
+  lib,
+  osConfig,
+  ...
+}:
+let
+  cfg = config.modules.applications.browsers.default;
+  headless = osConfig.modules.system.headless;
+in
 {
   imports = [
     ./firefox
+  ];
+
+  options.modules.applications.browsers.default = lib.mkOption {
+    type = lib.types.nullOr lib.types.str;
+    default = null;
+    description = "The default browser for the system.";
+  };
+
+  config.assertions = lib.mkIf (!headless.enable) [
+    {
+      assertion = cfg != null;
+      message = ''The "modules.applications.browsers.default" option must be defined.'';
+    }
+    {
+      assertion = config.modules.applications.browsers.${cfg}.enable;
+      message = "The default browser must be enabled.";
+    }
   ];
 }
