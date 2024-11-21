cf6e0fd0de0a7e025010d8e3a88af8b8a6a68e9a
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-20 23:28:56 -0600

    feat(monitoring): import some monitoring tools

--- homeManagerModules/applications/default.nix
+++ homeManagerModules/applications/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./browsers
+    ./monitoring
     ./terminals
   ];
 }
---
+++ homeManagerModules/applications/monitoring/default.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.modules.applications.monitoring.default;
+in
+{
+  imports = [
+    ./btop
+    ./htop
+  ];
+
+  options.modules.applications.browsers.default = lib.mkOption {
+    type = lib.types.nullOr lib.types.str;
+    default = null;
+    description = "The default process monitoring tool for the system.";
+  };
+
+  config.assertions = [
+    {
+      assertion = cfg != null;
+      message = ''The "modules.applications.monitoring.default" option must be defined.'';
+    }
+  ];
+}
