103f5f1bf0e578f6f161d6ef5591292051a138ac
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-21 23:38:18 -0600

    feat(podman): add accompanying podman system packages

--- nixosModules/virtualisation/podman/default.nix
+++ nixosModules/virtualisation/podman/default.nix
@@ -1,11 +1,26 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.modules.virtualisation;
+
+  headless = config.modules.system.headless;
 in
 {
   config = lib.mkIf cfg.enable {
     virtualisation.podman.enable = true;
 
     virtualisation.podman.dockerCompat = true;
+
+    environment.systemPackages =
+      with pkgs;
+      [
+        podman-tui
+        podman-compose
+      ]
+      ++ lib.optionals (!headless) [ podman-desktop ];
   };
 }
