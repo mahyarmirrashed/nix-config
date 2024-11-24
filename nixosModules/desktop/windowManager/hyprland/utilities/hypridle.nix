f7647373bf0b95a73d68bd4bf358e4673deb7e65
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-11-24 01:10:47 -0600

    feat(hyprland): add hypridle utility

---
+++ nixosModules/desktop/windowManager/hyprland/utilities/hypridle.nix
@@ -0,0 +1,41 @@
+{ config, lib, ... }:
+let
+  cfg = config.modules.desktop.windowManager.hyprland;
+in
+{
+  config = lib.mkIf cfg.enable {
+
+    services.hypridle = {
+      enable = true;
+
+      settings = {
+        general = {
+          lock_cmd = "pidof hyprlock || hyprlock";
+          before_sleep_cmd = "loginctl lock-session";
+          after_sleep_cmd = "hyprctl dispatch dpms on";
+        };
+
+        listener = [
+          {
+            timeout = 150;
+            on-timeout = "brightnessctl set 0 --save && brightnessctl --device=tpacpi::kbd_backlight set 0 --save";
+            on-resume = "brightnessctl --restore && brightnessctl --device=tpacpi::kbd_backlight --restore";
+          }
+          {
+            timeout = 300;
+            on-timeout = "loginctl lock-session";
+          }
+          {
+            timeout = 360;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+          {
+            timeout = 1800;
+            on-timeout = "systemctl suspend";
+          }
+        ];
+      };
+    };
+  };
+}
