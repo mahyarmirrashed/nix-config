195c8ba58fbfe40b9c1c59aa33be03eaa9bce947
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-01-22 16:56:55 -0600

    refactor(hm): consolidate all hyprland configuration

--- homeManagerModules/desktop/default.nix
+++ homeManagerModules/desktop/default.nix
@@ -3,7 +3,6 @@
   imports = [
     ./fileManager
     ./launcher
-    ./locker
     ./windowManager
   ];
 }
--- homeManagerModules/desktop/windowManager/default.nix
+++ homeManagerModules/desktop/windowManager/default.nix
@@ -1,6 +1,65 @@
-{ ... }:
+{
+  config,
+  lib,
+  osConfig,
+  ...
+}:
+let
+  headless = osConfig.modules.system.headless;
+
+  browser = config.modules.applications.browser.default;
+  terminal = config.modules.applications.terminal.default;
+  launcher = config.modules.desktop.launcher.default;
+in
 {
   imports = [
-    ./hyprland
+    ./utilities
   ];
+
+  config = lib.mkIf (!headless.enable) {
+    programs.hyprlock.settings = {
+      general = {
+        disable_loading_bar = true;
+      };
+    };
+
+    wayland.windowManager.hyprland = lib.mkIf (!headless.enable) {
+      enable = true;
+
+      settings = {
+        "$mod" = lib.mkDefault "Super_L";
+        "$terminal" = terminal;
+        "$browser" = browser;
+        "$launcher" = launcher;
+        "$locker" = "hyprlock";
+
+        exec-once = [ "hyprpaper" ];
+
+        # XXX: remove after configurations
+        debug = {
+          disable_logs = false;
+        };
+
+        input = {
+          touchpad = {
+            natural_scroll = true;
+          };
+        };
+
+        misc = {
+          disable_hyprland_logo = true;
+          disable_splash_rendering = true;
+        };
+
+        bindd = [
+          "$mod, T, Open terminal, exec, $terminal"
+          "$mod, grave, Open terminal, exec, $terminal"
+          "$mod, E, Open browser, exec, $browser"
+          "$mod, W, Close current window, killactive,"
+          "$mod, L, Lock computer, exec, $locker"
+          "$mod, space, Open launcher, exec, $launcher"
+        ];
+      };
+    };
+  };
 }
