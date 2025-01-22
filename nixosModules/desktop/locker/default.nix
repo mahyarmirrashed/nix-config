9719204220ab34814eeb0e69abe2689d84883c99
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-01-22 17:03:35 -0600

    refactor(nixos): move hyprlock configuration as sole locker

--- nixosModules/desktop/locker/default.nix
+++ nixosModules/desktop/locker/default.nix
@@ -1,6 +1,15 @@
-{ ... }:
 {
-  imports = [
-    ./hyprlock
-  ];
+  config,
+  lib,
+  ...
+}:
+let
+  headless = config.modules.system.headless.enable;
+in
+{
+  config = lib.mkIf (!headless.enable) {
+    programs.hyprlock.enable = true;
+
+    security.pam.services.hyprlock = { };
+  };
 }
