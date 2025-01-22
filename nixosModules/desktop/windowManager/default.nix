c3ce50d40a9a811b55eef4d4a26119ec820b7699
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-01-22 17:04:53 -0600

    refactor(nixos): move hyprland as only window manager

--- nixosModules/desktop/windowManager/default.nix
+++ nixosModules/desktop/windowManager/default.nix
@@ -1,6 +1,25 @@
-{ ... }:
 {
-  imports = [
-    ./hyprland
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  headless = config.modules.system.headless.enable;
+in
+{
+  config = lib.mkIf (!headless.enable) {
+    programs.hyprland.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      brightnessctl
+    ];
+
+    environment.sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+      XDG_CURRENT_DESKTOP = "Hyprland";
+      XDG_SESSION_DESKTOP = "Hyprland";
+      XDG_SESSION_TYPE = "wayland";
+    };
+  };
 }
