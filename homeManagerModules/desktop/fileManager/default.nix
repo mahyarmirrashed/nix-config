64fa801f0c467ad990b274533f5d010d33225624
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-01-22 16:18:05 -0600

    refactor: consolidate configurations for file managers

--- homeManagerModules/desktop/fileManager/default.nix
+++ homeManagerModules/desktop/fileManager/default.nix
@@ -1,32 +1,15 @@
 {
   config,
   lib,
-  osConfig,
+  pkgs,
   ...
 }:
-let
-  cfg = config.modules.desktop.fileManager.default;
-  headless = osConfig.modules.system.headless;
-in
 {
-  imports = [
-    ./yazi
-  ];
-
-  options.modules.desktop.fileManager.default = lib.mkOption {
-    type = lib.types.nullOr lib.types.str;
-    default = null;
-    description = "The default file manager for the system.";
+  config = {
+    home.packages = with pkgs; [
+      yazi
+    ];
   };
 
-  config.assertions = lib.mkIf (!headless.enable) [
-    {
-      assertion = cfg != null;
-      message = ''The "modules.desktop.fileManager.default" option must be defined.'';
-    }
-    {
-      assertion = config.modules.desktop.fileManager.${cfg}.enable;
-      message = "The default file manager must be enabled.";
-    }
-  ];
+  options.modules.desktop.fileManager.default = lib.mkDefault "yazi";
 }
