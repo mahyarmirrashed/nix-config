6d2301a49fb040d0dc073b776a51559a7dabfe2c
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-01-22 16:16:59 -0600

    refactor: consolidate launcher configurations

--- homeManagerModules/desktop/launcher/default.nix
+++ homeManagerModules/desktop/launcher/default.nix
@@ -2,32 +2,19 @@
   config,
   lib,
   osConfig,
+  pkgs,
   ...
 }:
 let
-  cfg = config.modules.desktop.launcher.default;
   headless = osConfig.modules.system.headless;
 in
 {
-  imports = [
-    ./anyrun
-    ./rofi
-  ];
-
-  options.modules.desktop.launcher.default = lib.mkOption {
-    type = lib.types.nullOr lib.types.str;
-    default = null;
-    description = "The default launcher for the system.";
+  config = lib.mkIf (!headless.enable) {
+    home.packages = with pkgs; [
+      anyrun
+      rofi
+    ];
   };
 
-  config.assertions = lib.mkIf (!headless.enable) [
-    {
-      assertion = cfg != null;
-      message = ''The "modules.desktop.launcher.default" option must be defined.'';
-    }
-    {
-      assertion = config.modules.desktop.launcher.${cfg}.enable;
-      message = "The default launcher must be enabled.";
-    }
-  ];
+  options.modules.desktop.launcher.default = lib.mkDefault "anyrun";
 }
