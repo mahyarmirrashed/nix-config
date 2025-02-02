bd2efca655324cc4309c4bec8b9231b1bdcd54a0
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-02-02 01:16:52 -0600

    feat(dev): configure git and gh

--- homeManagerModules/applications/development/default.nix
+++ homeManagerModules/applications/development/default.nix
@@ -7,16 +7,49 @@
 {
   config = {
     home.packages = with pkgs; [
-      git
-      gh
-      jj
-
-      neovim
+      jujutsu
       lazygit
+      neovim
     ];
 
-    programs.neovim = {
-      defaultEditor = true;
+    programs.git = {
+      enable = true;
+      userName = "Mahyar Mirrashed";
+      userEmail = "mah.mirr@gmail.com";
+
+      aliases = {
+        fuckit = "!${lib.getExe pkgs.git} reset --hard && ${lib.getExe pkgs.git} clean -fdx";
+      };
+
+      extraConfig = {
+        core.editor = "nvim";
+        core.eol = "lf";
+        core.autocrlf = "input";
+        color.ui = "auto";
+        fetch.prune = true;
+        init.defaultBranch = "main";
+        push.default = "upstream";
+        rebase.updateRefs = true;
+      };
+
+      ignores = [
+        "*~"
+        "*.swp"
+        "*.bak"
+        ".DS_Store" # MacOS Finder files
+      ];
+
+      ## Extraneous Git programs
+
+      delta.enable = true;
+    };
+
+    programs.gh = {
+      enable = true;
+      settings = {
+        editor = "nvim";
+        git_protocol = "ssh";
+      };
     };
   };
 }
