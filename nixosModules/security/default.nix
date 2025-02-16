88902da0b10ec46957576bd78093f0c33d59fde8
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-02-15 22:20:12 -0600

    feat(wm): add udev rules for libinput-gestures

--- nixosModules/security/default.nix
+++ nixosModules/security/default.nix
@@ -1,4 +1,9 @@
 { ... }:
 {
   security.pam.enable = true;
+
+  services.udev.extraRules = ''
+    # Apply uaccess for touchpad devices
+    KERNEL=="event*", SUBSYSTEM=="input", ENV{ID_INPUT_TOUCHPAD}=="1", MODE="0660", TAG+="uaccess"
+  '';
 }
