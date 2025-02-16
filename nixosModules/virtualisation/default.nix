f45cb2eabf5b594a6971a985cc1ad56537863b0d
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-02-15 21:45:28 -0600

    refactor(nixos): refactor virtualization configurations

--- nixosModules/virtualisation/default.nix
+++ nixosModules/virtualisation/default.nix
@@ -1,8 +1,30 @@
-{ lib, ... }:
 {
-  imports = [
-    ./containers
-  ];
-
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.modules.virtualisation;
+  headless = config.modules.system.headless;
+in
+{
   options.modules.virtualisation.enable = lib.mkEnableOption "virtualisation";
+
+  config = lib.mkIf cfg.enable {
+    virtualisation.podman.enable = true;
+
+    virtualisation.podman.dockerCompat = true;
+
+    environment.systemPackages =
+      with pkgs;
+      [
+        podman-tui
+        podman-compose
+
+        buildah
+        skopeo
+      ]
+      ++ lib.optionals (!headless.enable) [ podman-desktop ];
+  };
 }
