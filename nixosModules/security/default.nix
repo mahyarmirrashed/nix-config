dea59d7f591d38fed7dfd39587423bfc92d80da0
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-02-16 09:11:47 -0600

    fix(sec): properly set udev rules for libinput-gestures

--- nixosModules/security/default.nix
+++ nixosModules/security/default.nix
@@ -2,8 +2,14 @@
 {
   security.pam.enable = true;
 
-  services.udev.extraRules = ''
-    # Apply uaccess for touchpad devices
-    KERNEL=="event*", SUBSYSTEM=="input", ENV{ID_INPUT_TOUCHPAD}=="1", MODE="0660", TAG+="uaccess"
-  '';
+  services.udev.packages = lib.singleton (
+    pkgs.writeTextFile {
+      name = "libinput-gestures-udev-rules";
+      text = ''
+        # Grant ACL access for touchpad devices specifically for libinput-gestures
+        KERNEL=="event*", SUBSYSTEM=="input", ENV{ID_INPUT_TOUCHPAD}=="1", MODE="0660", TAG+="uaccess"
+      '';
+      destination = "/etc/udev/rules.d/70-libinput-gestures.rules";
+    }
+  );
 }
